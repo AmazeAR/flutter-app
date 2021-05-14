ae144dd12f22f2a718e80a81ec44cb784223d25c
Sagar Suman <sagarsuman1299@gmail.com>
2021-05-14 12:50:45 +0530

    feature/sign-in

--- lib/Screens/SignIn.dart
+++ lib/Screens/SignIn.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_amaze_ar/Components/RectangleButton.dart';
@@ -8,21 +6,6 @@ import 'package:flutter_amaze_ar/Constants/Colors.dart';
 import 'package:flutter_amaze_ar/Constants/Sizes.dart';
 import 'package:flutter_amaze_ar/Screens/Categories.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
-// GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['profile', 'email']);
-GoogleSignIn _googleSignIn = GoogleSignIn(
-  scopes: [
-    'email',
-    'https://www.googleapis.com/auth/contacts.readonly',
-  ],
-);
-// Future<void> _handleSignIn() async {
-//   try {
-//     await _googleSignIn.signIn();
-//   } catch (error) {
-//     print(error);
-//   }
-// }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({
@@ -34,116 +17,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  GoogleSignInAccount? _currentUser;
-  String _contactText = '';
+  bool _isLoggedIn = false;
+  GoogleSignInAccount? _userObj;
+  GoogleSignIn _googleSignIn = GoogleSignIn();
 
   @override
   void initState() {
     super.initState();
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
-      setState(() {
-        _currentUser = account;
-      });
-      if (_currentUser != null) {
-        _handleGetContact(_currentUser!);
-      }
-    });
-    _googleSignIn.signInSilently();
-  }
-
-  Future<void> _handleGetContact(GoogleSignInAccount user) async {
     setState(() {
-      _contactText = "Loading contact info...";
-    });
-    final http.Response response = await http.get(
-      Uri.parse('https://people.googleapis.com/v1/people/me/connections'
-          '?requestMask.includeField=person.names'),
-      headers: await user.authHeaders,
-    );
-    if (response.statusCode != 200) {
-      setState(() {
-        _contactText = "People API gave a ${response.statusCode} "
-            "response. Check logs for details.";
-      });
-      print('People API ${response.statusCode} response: ${response.body}');
-      return;
-    }
-    final Map<String, dynamic> data = json.decode(response.body);
-    final String? namedContact = _pickFirstNamedContact(data);
-    setState(() {
-      if (namedContact != null) {
-        _contactText = "I see you know $namedContact!";
-      } else {
-        _contactText = "No contacts to display.";
+      if (_isLoggedIn) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return Categories();
+            },
+          ),
+        );
       }
     });
-  }
-
-  String? _pickFirstNamedContact(Map<String, dynamic> data) {
-    final List<dynamic>? connections = data['connections'];
-    final Map<String, dynamic>? contact = connections?.firstWhere(
-      (dynamic contact) => contact['names'] != null,
-      orElse: () => null,
-    );
-    if (contact != null) {
-      final Map<String, dynamic>? name = contact['names'].firstWhere(
-        (dynamic name) => name['displayName'] != null,
-        orElse: () => null,
-      );
-      if (name != null) {
-        return name['displayName'];
-      }
-    }
-    return null;
-  }
-
-  Future<void> _handleSignIn() async {
-    try {
-      await _googleSignIn.signIn();
-    } catch (error) {
-      print(error);
-    }
-  }
-
-  Future<void> _handleSignOut() => _googleSignIn.disconnect();
-
-  Widget _buildBody() {
-    GoogleSignInAccount? user = _currentUser;
-    if (user != null) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          ListTile(
-            leading: GoogleUserCircleAvatar(
-              identity: user,
-            ),
-            title: Text(user.displayName ?? ''),
-            subtitle: Text(user.email),
-          ),
-          const Text("Signed in successfully."),
-          Text(_contactText),
-          ElevatedButton(
-            child: const Text('SIGN OUT'),
-            onPressed: _handleSignOut,
-          ),
-          ElevatedButton(
-            child: const Text('REFRESH'),
-            onPressed: () => _handleGetContact(user),
-          ),
-        ],
-      );
-    } else {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          const Text("You are not currently signed in."),
-          ElevatedButton(
-            child: const Text('SIGN IN'),
-            onPressed: _handleSignIn,
-          ),
-        ],
-      );
-    }
   }
 
   @override
@@ -175,20 +67,38 @@ class _MyHomePageState extends State<MyHomePage> {
                     fontWeight: FontWeight.bold),
               ),
               SizedBox(height: size.height * 0.1),
-              RectangleButton(
-                text: "SignIn With Google ",
-                color: primaryColor,
-                onTap: () {
-                  Navigator.push(
-                    //
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return Categories();
-                      },
-                    ),
-                  );
-                },
+              Container(
+                child: _isLoggedIn
+                    ? RectangleButton(
+                        text: "SignOut With Google ",
+                        color: primaryColor,
+                        onTap: () {
+                          print("isloogedin : $_isLoggedIn");
+                          _googleSignIn.signOut().then((value) {
+                            setState(() {
+                              _isLoggedIn = false;
+                            });
+                          }).catchError((onError) {
+                            print(onError);
+                          });
+                        },
+                      )
+                    : RectangleButton(
+                        text: "SignIn With Google ",
+                        color: primaryColor,
+                        onTap: () {
+                          print("isloogedin : $_isLoggedIn");
+                          _googleSignIn.signIn().then((value) {
+                            setState(() {
+                              _isLoggedIn = true;
+                              _userObj = value;
+                            });
+                          }).catchError((onError) {
+                            print(onError);
+                          });
+                          if (_isLoggedIn) {}
+                        },
+                      ),
               )
             ],
           ),
