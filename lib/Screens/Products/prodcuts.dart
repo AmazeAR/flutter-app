b924a46c964d701b4f653b0fab0d68e59b907fdf
Manish-git-135 <mmanishmoond2000@gmail.com>
2021-05-21 19:43:58 +0530

    api integrated for products page and card

--- lib/Screens/Products/prodcuts.dart
+++ lib/Screens/Products/prodcuts.dart
@@ -11,22 +11,35 @@ class Products extends StatelessWidget {
       appBar: AppBar(
         title: Text("Products"),
       ),
-      body: FutureBuilder(
-          future: httpService.getPro(),
-          builder:
-              (BuildContext context, AsyncSnapshot<List<Product>> snapshot) {
-            if (snapshot.hasData) {
-              List<Product> pros = snapshot.data!;
-              return ListView(
-                children: pros
-                    .map((Product pro) => ProductCard())
-                    .toList(),
+      body: Container(
+        color: Colors.blue.shade100,
+        child: FutureBuilder(
+            future: httpService.getPro(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Product>> snapshot) {
+              if (snapshot.hasData) {
+                List<Product> pros = snapshot.data!;
+                return GridView.count(
+                  padding: EdgeInsets.all(6),
+                  crossAxisCount: 2,
+                  children: pros
+                      .map((Product pro) => ProductCard(
+                          id: pro.id,
+                          catId: pro.catId,
+                          catName: pro.catName,
+                          proName: pro.proName,
+                          brandName: pro.brandName,
+                          proImage: pro.proImage,
+                          price: pro.price,
+                          is3DModel: pro.is3DModel))
+                      .toList(),
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(),
               );
-            }
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }),
+            }),
+      ),
     );
   }
 }
--- lib/Screens/Products/product_card.dart
+++ lib/Screens/Products/product_card.dart
@@ -1,10 +1,78 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
+  final String id;
+  final String catId;
+  final String catName;
+  final String proName;
+  final String brandName;
+  final String proImage;
+  final String price;
+  final bool is3DModel;
+
+  const ProductCard(
+      {Key? key,
+      required this.id,
+      required this.catId,
+      required this.catName,
+      required this.proName,
+      required this.brandName,
+      required this.proImage,
+      required this.price,
+      required this.is3DModel})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Manish"),
-    );
+        child: Card(
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: () {
+          print('Card tapped.');
+        },
+        child: Column(
+          children: [
+            Image.network(
+              proImage,
+              height: 100,
+              width: 100,
+            ),
+            SizedBox(
+              width: 120.0,
+              child: Text(
+                proName,
+                maxLines: 1,
+                overflow: TextOverflow.fade,
+                softWrap: false,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0),
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  price,
+                  style: TextStyle(fontSize: 16),
+                ),
+                Icon(
+                  Icons.circle,
+                  color: is3DModel == true ? Colors.green : Colors.red,
+                  size: 16,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
