7d409516b4057222fde94528e5eee43167ca0c3c
KAPIL <kapil992000@gmail.com>
2021-05-27 00:58:55 +0530

    completed personalCart except geting userId from localstorage

--- lib/Screens/Personal_Cart.dart
+++ lib/Screens/Personal_Cart.dart
@@ -2,11 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_amaze_ar/Components/Personal_Cart_Card.dart';
 import 'package:flutter_amaze_ar/Components/appbar_with_profile.dart';
 import 'package:flutter_amaze_ar/models/product_model.dart';
-import 'package:flutter_amaze_ar/services/personal_cart_services.dart';
+import 'package:flutter_amaze_ar/services/personalCart_services.dart';
 
-class PersonalCart extends StatelessWidget {
-  final HttpServicePersonalCart _httpService =
-      HttpServicePersonalCart(userid: '609cfe4632e2cc480c27106d');
+class PersonalCart extends StatefulWidget {
+  @override
+  _PersonalCartState createState() => _PersonalCartState();
+}
+
+class _PersonalCartState extends State<PersonalCart> {
+  final HttpPersonalCartServices _httpService =
+      HttpPersonalCartServices(userId: '609cfe4632e2cc480c27106d');
+
+  late Future<List<ProductModel>> cartFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    cartFuture = _httpService.getPersoanlCart();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,13 +29,13 @@ class PersonalCart extends StatelessWidget {
         child: AppBarWithProfileIcon(),
       ),
       body: FutureBuilder(
-        future: _httpService.getPersoanlCart(),
+        future: cartFuture,
         builder:
             (BuildContext context, AsyncSnapshot<List<ProductModel>> snapshot) {
           if (snapshot.hasData) {
-            List<ProductModel> personalCartPro = snapshot.data!;
+            List<ProductModel> cart = snapshot.data!;
             return ListView(
-              children: personalCartPro
+              children: cart
                   .map((ProductModel item) => PersonalCartCard(
                       id: item.productId,
                       catId: item.categoryId,
@@ -32,6 +46,10 @@ class PersonalCart extends StatelessWidget {
                       price: item.price,
                       is3DModel: item.is3DModel))
                   .toList(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text("${snapshot.error}"),
             );
           }
           return Center(
---
+++ lib/services/personalCart_services.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:flutter_amaze_ar/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class HttpPersonalCartServices {
+  final String userId;
+
+  HttpPersonalCartServices({required this.userId});
+
+  Future<List<ProductModel>> getPersoanlCart() async {
+    final res = await http
+        .get(Uri.https('amazar-v1.herokuapp.com', '/personalCart/$userId'));
+
+    // print(res.statusCode);
+    if (res.statusCode == 200) {
+      print("personal cart successfuly fetched!");
+
+      var body = json.decode(res.body);
+      List<dynamic> cartJson = body['data']; // need a check for empty cart
+
+      List<ProductModel> cart = cartJson
+          .map((dynamic cartItem) => ProductModel.fromJson(cartItem))
+          .toList();
+
+      return cart;
+    } else {
+      throw Exception("Failed to fetch personalCart from personalCart/user_id");
+    }
+  }
+}
